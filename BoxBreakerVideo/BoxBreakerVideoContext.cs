88d43ea032c91ffdc26f17b852487deb33eac487
cheesy d <cheesyd64@hotmail.com>
2023-02-04 20:29:47 -0800

    Created user login fields

---
+++ BoxBreakerVideo/BoxBreakerVideoContext.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using BoxBreakerVideo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BoxBreakerVideo;
+
+public partial class BoxBreakerVideoContext : DbContext
+{
+    public BoxBreakerVideoContext()
+    {
+    }
+
+    public BoxBreakerVideoContext(DbContextOptions<BoxBreakerVideoContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Member> Members { get; set; }
+
+    public virtual DbSet<MemberMovie> MemberMovies { get; set; }
+
+    public virtual DbSet<Movie> Movies { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source =(localdb)\\MSSQLLocalDb;Initial Catalog = BoxBreakerVideo");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Member>(entity =>
+        {
+            entity.HasKey(e => e.MemberId).HasName("PK__Members__0CF04B18E3BA3C88");
+
+            entity.HasIndex(e => e.MemberEmail, "UQ__Members__3F37B77ADB487270").IsUnique();
+
+            entity.HasIndex(e => e.MemberPhone, "UQ__Members__8B704E8BF8615F45").IsUnique();
+
+            entity.HasIndex(e => e.MemberAddress, "UQ__Members__9FE4ACC3F5C92740").IsUnique();
+
+            entity.Property(e => e.MemberAddress)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.MemberEmail)
+                .HasMaxLength(250)
+                .IsUnicode(false);
+            entity.Property(e => e.MemberFname)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("MemberFName");
+            entity.Property(e => e.MemberLname)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("MemberLName");
+            entity.Property(e => e.MemberPassword)
+                .HasMaxLength(200)
+                .IsUnicode(false);
+            entity.Property(e => e.MemberPhone)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<MemberMovie>(entity =>
+        {
+            entity.HasKey(e => new { e.MovieId, e.MemberId }).HasName("PK__MemberMo__DB1D90AB51A88CB5");
+
+            entity.Property(e => e.CheckoutDate).HasColumnType("datetime");
+            entity.Property(e => e.DueDate).HasColumnType("date");
+
+            entity.HasOne(d => d.Member).WithMany(p => p.MemberMovies)
+                .HasForeignKey(d => d.MemberId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__MemberMov__Membe__2C3393D0");
+
+            entity.HasOne(d => d.Movie).WithMany(p => p.MemberMovies)
+                .HasForeignKey(d => d.MovieId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__MemberMov__Movie__2B3F6F97");
+        });
+
+        modelBuilder.Entity<Movie>(entity =>
+        {
+            entity.HasKey(e => e.MovieId).HasName("PK__Movie__4BD2941ABBE7F478");
+
+            entity.ToTable("Movie");
+
+            entity.Property(e => e.Genre)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.MaturityRating)
+                .HasMaxLength(10)
+                .IsUnicode(false)
+                .IsFixedLength();
+            entity.Property(e => e.MovieDescription)
+                .HasMaxLength(500)
+                .IsUnicode(false);
+            entity.Property(e => e.MoviePoster)
+                .HasMaxLength(1000)
+                .IsUnicode(false);
+            entity.Property(e => e.MoviePrice).HasColumnType("smallmoney");
+            entity.Property(e => e.MovieRuntime)
+                .HasMaxLength(10)
+                .IsUnicode(false);
+            entity.Property(e => e.ReleaseDate).HasColumnType("date");
+            entity.Property(e => e.Title)
+                .HasMaxLength(150)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
--- BoxBreakerVideo/Form1.Designer.cs
+++ BoxBreakerVideo/Form1.Designer.cs
@@ -23,12 +23,89 @@
 		///  the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(67, 268);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(182, 66);
+            this.btnLogin.TabIndex = 0;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(289, 268);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(178, 66);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "User Email";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(161, 67);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(306, 27);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(161, 148);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(306, 27);
+            this.textBox2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnLogin);
+            this.Name = "Form1";
+            this.Text = "User Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
 		}
 
-		#endregion
-	}
+        #endregion
+
+        private Button btnLogin;
+        private Button btnCancel;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+    }
 }
