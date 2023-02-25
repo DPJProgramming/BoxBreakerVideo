27c4e8807d6adc56514aac8e5c43b958b36bfddd
cheesy d <cheesyd64@hotmail.com>
2023-02-24 19:32:10 -0800

    Completed Delete Movie Functionality

--- BoxBreakerVideo/Forms/AdminForm.cs
+++ BoxBreakerVideo/Forms/AdminForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BoxBreakerVideo.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -20,6 +21,30 @@ namespace BoxBreakerVideo.Forms {
 
         private void button5_Click(object sender, EventArgs e) {
             Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+
+            //make new connection to database
+            BoxBreakerVideoContext database = new BoxBreakerVideoContext();
+
+            //make a movie object from the database row that matches the text box input. If there is a match, delete that movie
+            //if not, show an error
+            try {
+                Movie movieToDelete = database.Movies.Where(movie => movie.Title == txtbxDeleteMovie.Text).First();
+                database.Remove(movieToDelete);
+                MessageBox.Show($"{txtbxDeleteMovie.Text} Deleted Succesfully");
+                database.SaveChanges();
+                txtbxDeleteMovie.Clear();
+            }
+            catch {
+                MessageBox.Show("Movie Not Found");
+            }
+
+
+
+
+
         }
     }
 }
