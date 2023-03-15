320a321bd6139c8648c6f1f1ad892d1331eab068
cheesy d <cheesyd64@hotmail.com>
2023-03-14 23:22:18 -0700

    Account page shows all movies checked out by logged in member

--- BoxBreakerVideo/Forms/MemberAccount.cs
+++ BoxBreakerVideo/Forms/MemberAccount.cs
@@ -13,19 +13,20 @@ using System.Windows.Forms;
 
 namespace BoxBreakerVideo.Forms {
     public partial class MemberAccount : Form {
-        public MemberAccount(Member member, BoxBreakerVideoContext database) {
+        public MemberAccount(Member loggedInMember, BoxBreakerVideoContext database) {
             InitializeComponent();
 
 
-            //For welcome message
-            lblFirstName.Text = member.MemberFname;
+            //For welcome message showing members first name
+            lblFirstName.Text = loggedInMember.MemberFname;
             lstbxMovieList.Items.Clear();
 
-            List<MemberMovie> movieList = database.MemberMovies.Where(m => m.MemberId == member.MemberId).ToList();
+            //create list of memberMovie objects attached to loggedInMember
+            List<MemberMovie> movieList = database.MemberMovies.Where(m => m.MemberId == loggedInMember.MemberId).ToList();
 
             foreach (MemberMovie movie in movieList) {
 
-                string movieTitle = (from m in database.Movies where m.MovieId == movie.MovieId select m.Title).ToString();
+                string? movieTitle = (from m in database.Movies where m.MovieId == movie.MovieId select m.Title).SingleOrDefault();
 
                 //string title = database.Movies.Where(m => m.MovieId == movie.MovieId)
                 lstbxMovieList.Items.Add(movieTitle + "          " + movie.CheckoutDate.Date + "        " + "       " + movie.DueDate.Date);
